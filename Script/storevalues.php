d9854712ed55c00e43d0141fa0e185f81784fd9c
Michael Dayah <michael@dayah.com>
2012-05-31 21:51:31 -0400

    Replace REPLACE INTO with INSERT ... ON DUPLICATE KEY.

--- Script/storevalues.php
+++ Script/storevalues.php
@@ -1,16 +1,15 @@
-<?
-include "functions.php";
+<? include "functions.php";
 
 if (isset($_SESSION["valid"]) && $_SESSION["valid"] === 1) {
 	$conn = database_connect();
 	$id = $_SESSION["id"];
-	print_r($_POST);
 
 	foreach ($_POST as $key => $val) {
 		if ($val != "") {
 			$pair = explode(":", $key);
 			$date = date_create_from_format("Y-m-d", $pair[1]);
-			$query = "REPLACE INTO calories (id, date, $pair[0]) VALUES ($id, '" . $date->format('Y-m-d') . "', $val);";
+			$query = "INSERT INTO calories (id, date, $pair[0]) VALUES ($id, '" . $date->format('Y-m-d') . "', $val) ON DUPLICATE KEY UPDATE $pair[0]=$val;";
+			echo $query;
 			mysqli_query($conn, $query);
 			echo $conn->error;
 		}
