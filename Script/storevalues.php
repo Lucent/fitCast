6d1df3a3d8cc2ffebd72c55b0a2f5024416a0c74
Michael Dayah <michael@dayah.com>
2012-06-07 18:30:37 -0400

    Insert all submitted values in one query.

--- Script/storevalues.php
+++ Script/storevalues.php
@@ -12,13 +12,15 @@ if (isset($_SESSION["valid"]) && $_SESSION["valid"] === 1) {
 		$submission[$pair[1]][$pair[0]] = $val == "" ? "NULL" : $val;
 	}
 
+	$values = array();
 	foreach ($submission as $date => $vals) {
 		$date = date_create_from_format("Y-m-d", $date)->format("Y-m-d");
-		$query = "INSERT INTO calories (id, date, food, exercise, measured) VALUES ($id, '$date', {$vals['food']}, {$vals['exercise']}, {$vals['measured']}) ON DUPLICATE KEY UPDATE food={$vals['food']}, exercise={$vals['exercise']}, measured={$vals['measured']};";
-		echo $query, "\n";
-		mysqli_query($conn, $query);
-		echo $conn->error;
+		$values[] = "($id, '$date', {$vals['food']}, {$vals['exercise']}, {$vals['measured']})";
 	}
+	$query = "REPLACE INTO calories (id, date, food, exercise, measured) VALUES " . implode(", ", $values) . ";";
+	echo $query;
+	mysqli_query($conn, $query);
+	echo $conn->error;
 
 	mysqli_close($conn);
 }
